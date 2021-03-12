3fe38cf18649ab5d7ea6ad1f047a527d1806449c
Sravas Melarkode <63852545+SupersonicCoder18@users.noreply.github.com>
2021-03-12 16:53:37 +0530

    Add files via upload
    Officially my first FPS!

---
+++ shoot.js
@@ -0,0 +1,37 @@
+AFRAME.registerComponent("bullets", {
+    init: function(){
+        this.shootBullet()
+    },
+
+    shootBullet: function(){
+        window.addEventListener("keydown", (e) => {
+            if (e.key === "z"){
+                var bullet = document.createElement("a-entity")
+                bullet.setAttribute("geometry", {
+                    primitive: "sphere",
+                    radius: 0.05
+                })
+            bullet.setAttribute("material", "color", "black")
+            var cam = document.querySelector("#camera")
+            pos = cam.getAttribute("position")
+            bullet.setAttribute("position", {
+                x: pos.x,
+                y: pos.y,
+                z: pos.z
+            })
+            bullet.setAttribute("velocity", {
+                x: 0,
+                y: 0,
+                z: -1
+            })
+            var camera = document.querySelector("#camera").object3D
+            var direction = new THREE.Vector3()
+            camera.getWorldDirection(direction)
+            console.log(direction)
+            bullet.setAttribute("velocity", direction.multiplyScalar(-10))
+            var scene = document.querySelector("#scene")
+            scene.appendChild(bullet)
+            }
+        })
+    }
+})
